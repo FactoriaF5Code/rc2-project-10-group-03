4aafaf6ab695d7e5885a7b83708962369a2084a1
osaga-fu <151243613+osaga-fu@users.noreply.github.com>
2024-03-20 15:08:39 +0100

    Deleted unused imports

--- backend/src/main/java/com/factoriaf5/backend/persistence/Activity.java
+++ backend/src/main/java/com/factoriaf5/backend/persistence/Activity.java
@@ -1,7 +1,5 @@
 package com.factoriaf5.backend.persistence;
 
-import java.util.UUID;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
--- backend/src/main/java/com/factoriaf5/backend/persistence/MemberRepository.java
+++ backend/src/main/java/com/factoriaf5/backend/persistence/MemberRepository.java
@@ -1,6 +1,5 @@
 package com.factoriaf5.backend.persistence;
 
-import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
