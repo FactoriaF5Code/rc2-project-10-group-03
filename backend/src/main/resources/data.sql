2a05824db873c1d752bf24fe615a9104e575bdf3
joyjos <enjoyjos@hotmail.com>
2024-03-20 14:49:49 +0100

    H2 console working

---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO activities(activity_name, room, day_of_the_week, time ) VALUES
+('Zumba', 'Sala 2', 'Lunes, Martes, Miércoles', '12:00'),
+('Mantenimiento', 'Sala 1', 'Martes, Jueves', '10:00'),
+('Pilates', 'Sala 4', 'Lunes, Miércoles', '08:30'),
+('GAP', 'Sala 3', 'Martes, Jueves, Viernes', '17:00'),
+('Crossfit', 'Sala 5', 'Lunes, Miércoles', '08:30')
+;
