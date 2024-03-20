06e44a3d63d9c66f8e43165ee44b9ad8025affb2
joyjos <enjoyjos@hotmail.com>
2024-03-20 15:14:29 +0100

    Days data.sql  changed

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,7 +1,7 @@
 INSERT INTO activities(activity_name, room, day_of_the_week, time ) VALUES
-('Zumba', 'Sala 2', 'Lunes, Martes, Miércoles', '12:00'),
-('Mantenimiento', 'Sala 1', 'Martes, Jueves', '10:00'),
-('Pilates', 'Sala 4', 'Lunes, Miércoles', '08:30'),
-('GAP', 'Sala 3', 'Martes, Jueves, Viernes', '17:00'),
-('Crossfit', 'Sala 5', 'Lunes, Miércoles', '08:30')
+('Zumba', 'Sala 2', 'Monday, Tuesday, Wednesday', '12:00'),
+('Mantenimiento', 'Sala 1', 'Tuesday, Thursday', '10:00'),
+('Pilates', 'Sala 4', 'Monday, Wednesday', '08:30'),
+('GAP', 'Sala 3', 'Tuesday, Thursday, Friday', '17:00'),
+('Crossfit', 'Sala 5', 'Monday, Wednesday', '08:30')
 ;
